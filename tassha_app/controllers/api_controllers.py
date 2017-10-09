eb252a163fe356c7239e3656405a63d44ea31958
blong1996 <blong1996@gmail.com>
2017-10-08 23:28:07 -0700

    add api.ai

--- tassha_app/controllers/api_controllers.py
+++ tassha_app/controllers/api_controllers.py
@@ -1,6 +1,18 @@
-from flask import Blueprint, request, json, make_response
+from flask import Blueprint, request, json, make_response, jsonify
+import os.path
+import sys
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
 
 tassha_api = Blueprint('tassha_app', __name__)
+
+CLIENT_ACCESS_TOKEN = '314e5c7a6ca94d459386983ea985236c'
 
 
 @tassha_api.route('/webhook', methods=['POST'])
@@ -29,16 +41,43 @@ def webhook():
     return r
 
 
-# def processRequest(req):
-    # if req.get("result").get("action") != "yahooWeatherForecast":
-    #     return {}
-    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    # yql_query = makeYqlQuery(req)
-    # if yql_query is None:
-    #     return {}
-    # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    # result = urlopen(yql_url).read()
-    # data = json.loads(result)
-    # res = makeWebhookResult(data)
-    # return res
+@tassha_api.route('/apiai/<string:query>', methods=['POST'])
+def api_ai(query):
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
+    apirequest = ai.text_request()
+
+    apirequest.lang = 'en'
+
+    apirequest.session_id = "somethin"
+
+    if query:
+        apirequest.query = query
+    else:
+        apirequest.query = "Hey Tassha"
+
+    # response = apirequest.getresponse()
+
+    response = json.loads(apirequest.getresponse().read().decode('utf-8'))
+    # res = jsonify(response)
+
+    status = response['status']['code']
+    if status == 200:
+        # Sending the textual response of the bot.
+        return response['result']['fulfillment']['speech']
+
+    else:
+        return "Sorry, I couldn't understand that question"
+
+# def processRequest(req):
+# if req.get("result").get("action") != "yahooWeatherForecast":
+#     return {}
+# baseurl = "https://query.yahooapis.com/v1/public/yql?"
+# yql_query = makeYqlQuery(req)
+# if yql_query is None:
+#     return {}
+# yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+# result = urlopen(yql_url).read()
+# data = json.loads(result)
+# res = makeWebhookResult(data)
+# return res
